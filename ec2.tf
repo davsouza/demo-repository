34b633b61ba459de60eb9cc47b1d49fc576c6ff4
davsouza <ds3899@gmail.com>
2021-03-04 17:02:18 -0500

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "dev" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t2.micro"
+  tags = {
+    name = "dev"
+  }
+}
